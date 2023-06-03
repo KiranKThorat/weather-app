7106086c7275dd2edf689200dcf01ca75e986e9f
kt <kthorat158@gmail.com>
2023-06-03 23:00:51 +0530

    index.css styling done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ReactDOM } from "react";
+import hotBg from './assets/hot.jpg';
+import coldBg from './assets/cold.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ backgroundImage: `url(${hotBg})`}}>
+    <div className="overlay">
+      <div className="container">
+        <div className="section section_inputs">
+          <input type="text" name="city" placeholder="Enter city name.." />
+          <button>℉</button>
+        </div>
+        <div className="section section_temperature">
+          <div className="description">
+            <h3>pune,india</h3>
+            <img src="http://openweathermap.org/img/wn/04n@2x.png" alt="weather"></img>
+            <h3>Cloudy</h3>
+          </div>
+          <div className="temperature">
+            <h1>28.7 °С</h1>
+          </div>
+        </div>
+        {/* bottom discription */}
+      </div>
+    </div>
     </div>
   );
 }
